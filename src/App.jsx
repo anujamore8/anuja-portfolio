1b3484dec7df2076976d5401b20da2020bc6c983
anujamore8 <anujamore288@gmail.com>
2026-07-04 22:44:05 +0530

    Complete hero section and project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import TechStack from "./components/TechStack/TechStack";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <TechStack />
     </>
   );
 }
